cb6dde47537befcc60102b8dfeec04efc1ef819f
liukiss <71025113+liukiss@users.noreply.github.com>
2026-09-25 16:01:09 +0800

    Add files via upload

---
+++ jar/bendi.php
@@ -0,0 +1,48 @@
+[
+  {
+    "name": "本地包",
+    "list": [
+      {
+        "name": "肥猫线上包0626",
+        "url": "http://47.120.41.246:8025/vip/jar/bendi/feimao/itvba.zip",
+        "icon": "https://fc4tn.baidu.com/it/u=350590754,192170890&fm=202?x=0&y=0&h=340&w=510&vh=340.00&vw=510.00&oh=340.00&ow=510.00",
+        "version": "当前版本自行更新"
+      },
+      {
+        "name": "讴歌线上包0626",
+        "url": " http://47.120.41.246:8025/vip/jar/bendi/ouge/itvba.zip",
+        "icon": "https://fc4tn.baidu.com/it/u=350590754,192170890&fm=202?x=0&y=0&h=340&w=510&vh=340.00&vw=510.00&oh=340.00&ow=510.00",
+        "version": "当前版本自行更新"
+      },
+      {
+        "name": "小米线上包0626",
+        "url": " http://47.120.41.246:8025/vip/jar/bendi/xiaomi/itvba.zip",
+        "icon": "https://fc4tn.baidu.com/it/u=350590754,192170890&fm=202?x=0&y=0&h=340&w=510&vh=340.00&vw=510.00&oh=340.00&ow=510.00",
+        "version": "当前版本自行更新"
+      }
+    ]
+  },
+  {
+    "name": "本地包",
+    "list": [
+      {
+        "name": "肥猫线上包0626",
+        "url": "http://47.120.41.246:8025/vip/jar/bendi/feimao/itvba.zip",
+        "icon": "https://fc4tn.baidu.com/it/u=350590754,192170890&fm=202?x=0&y=0&h=340&w=510&vh=340.00&vw=510.00&oh=340.00&ow=510.00",
+        "version": "当前版本自行更新"
+      },
+      {
+        "name": "讴歌线上包0626",
+        "url": " http://47.120.41.246:8025/vip/jar/bendi/ouge/itvba.zip",
+        "icon": "https://fc4tn.baidu.com/it/u=350590754,192170890&fm=202?x=0&y=0&h=340&w=510&vh=340.00&vw=510.00&oh=340.00&ow=510.00",
+        "version": "当前版本自行更新"
+      },
+      {
+        "name": "小米线上包0626",
+        "url": " http://47.120.41.246:8025/vip/jar/bendi/xiaomi/itvba.zip",
+        "icon": "hhttps://fc4tn.baidu.com/it/u=350590754,192170890&fm=202?x=0&y=0&h=340&w=510&vh=340.00&vw=510.00&oh=340.00&ow=510.00",
+        "version": "当前版本自行更新"
+      }
+    ]
+  }
+]
